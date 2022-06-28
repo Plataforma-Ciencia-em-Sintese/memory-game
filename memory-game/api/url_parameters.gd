9ab517ca1da44c809098eed334939239f2de3877
rafaelsescato <rafaelsescato@gmail.com>
2022-06-28 11:39:20 -0300

    refactoring url_parameters.gd

--- memory-game/api/url_parameters.gd
+++ memory-game/api/url_parameters.gd
@@ -4,7 +4,16 @@ class_name URLParameters #, res://class_name_icon.svg
 
 
 #  [DOCSTRING]
-"""This class takes parameters passed by URL."""
+"""This class takes parameters passed by URL"""
+
+"""Expected pattern for url parameters
+
+single parameter
+	?parameter=value 
+
+multiple parameters
+	?parameter1=value&parameter2=value
+"""
 
 
 #  [SIGNALS]
@@ -24,15 +33,15 @@ class_name URLParameters #, res://class_name_icon.svg
 
 #  [PRIVATE_VARIABLES]
 var _parameters: Dictionary = {} \
-		setget set_parameters, get_parameters
+		setget , get_parameters
 
 
 #  [ONREADY_VARIABLES]
 
 
 #  [OPTIONAL_BUILT-IN_VIRTUAL_METHOD]
-func _init() -> void:
-	_url_access_parameters()
+#func _init() -> void:
+#	pass
 
 
 #  [BUILT-IN_VURTUAL_METHOD]
@@ -46,26 +55,7 @@ func _init() -> void:
 
 
 #  [PUBLIC_METHODS]
-func set_parameters(new_parameters: Dictionary) -> void:
-	_parameters = new_parameters
-
-
 func get_parameters() -> Dictionary:
-	return _parameters
-
-
-#  [PRIVATE_METHODS]
-func _url_access_parameters() -> void:
-	"""
-	expected pattern for url parameters
-	---
-	single parameter
-	?parameter=value 
-	
-	multiple parameters
-	?parameter1=value&parameter2=value
-	"""
-	
 	var raw_string: String = ""
 	if str(OS.get_name()) == "HTML5":
 		# The Javascript Class only works for HTML5.
@@ -75,18 +65,16 @@ func _url_access_parameters() -> void:
 		var fake_url_parameters: String = "id=23391&trash=000&skip=1"
 		raw_string = fake_url_parameters
 	
-	#print(raw_string)
-	
 	var strings: PoolStringArray = raw_string.split("&")
 	
 	var parameters: Dictionary = {}
 	for item in strings:
 		parameters[item.split("=")[0]] = item.split("=")[1]
 	
-	
-	#print(parameters)
-	
-	set_parameters(parameters)
- 
+	return parameters
+
+
+#  [PRIVATE_METHODS]
+
 
 #  [SIGNAL_METHODS]
